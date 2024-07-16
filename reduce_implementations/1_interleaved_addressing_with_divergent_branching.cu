c9351d5a2cb7a53c29631e0c8ab7ccc050561eaf
Julian <julianezequielarnesino@gmail.com>
2024-07-16 16:45:36 +0000

    Add explanation on divergent branching

--- reduce_implementations/1_interleaved_addressing_with_divergent_branching.cu
+++ reduce_implementations/1_interleaved_addressing_with_divergent_branching.cu
@@ -11,7 +11,7 @@ __global__ void reduce_using_1_interleaved_addressing_with_divergent_branching(i
 
     // Do reduction in shared memory.
     for(unsigned int amountOfElementsReduced = 1; amountOfElementsReduced < blockDim.x; amountOfElementsReduced *= 2) {
-        if (threadBlockIndex % (2 * amountOfElementsReduced) == 0) {
+        if (threadBlockIndex % (2 * amountOfElementsReduced) == 0) {  // This instruction produces divergent branching.
             sharedData[threadBlockIndex] += sharedData[threadBlockIndex + amountOfElementsReduced];
         }
         __syncthreads();
@@ -20,3 +20,15 @@ __global__ void reduce_using_1_interleaved_addressing_with_divergent_branching(i
     // Write this block's result in shared memory.
     if (threadBlockIndex == 0) outputData[blockIdx.x] = sharedData[0];
 }
+
+/*
+
+A warp is a set of 32 threads inside a block, and they all share the program counter. They all execute the same instruction at the same time.
+Divergent branching happens when threads in the same warp follow different execution paths.
+The divergent branching is produced when the if statment checks for (threadBlockIndex % (2 * amountOfElementsReduced) == 0).
+
+When threads in a warp diverge, the warp serializes the execution of different paths.
+Some threads in the warp are executing while others aren't.
+It introduces additional overhead.
+
+*/
