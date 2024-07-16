8ceceb22689aab95acf4138784a77555a05bbc90
Julian <julianezequielarnesino@gmail.com>
2024-07-16 17:55:05 +0000

    Update explanation for the first reduce implementation

--- reduce_implementations/1_interleaved_addressing_with_divergent_branching.cu
+++ reduce_implementations/1_interleaved_addressing_with_divergent_branching.cu
@@ -25,10 +25,13 @@ __global__ void reduce_using_1_interleaved_addressing_with_divergent_branching(i
 
 A warp is a set of 32 threads inside a block, and they all share the program counter. They all execute the same instruction at the same time.
 Divergent branching happens when threads in the same warp follow different execution paths.
-The divergent branching is produced when the if statment checks for (threadBlockIndex % (2 * amountOfElementsReduced) == 0).
 
 When threads in a warp diverge, the warp serializes the execution of different paths.
 Some threads in the warp are executing while others aren't.
 It introduces additional overhead.
+
+The divergent branching here is produced when the if statment checks for (threadBlockIndex % (2 * amountOfElementsReduced) == 0).
+Only one in every (2 * amountOfElementsReduced) consecutive threads is running the instructions inside the if statement.
+Therefore, not all threads inside the warp are running that instruction, leading to divergent branching.
 
 */
