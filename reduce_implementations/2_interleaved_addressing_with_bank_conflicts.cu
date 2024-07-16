ebd19e16d67b1b3227b7a202e9f66c659032f46c
Julian <julianezequielarnesino@gmail.com>
2024-07-16 18:03:27 +0000

    Add explanation for second reduce implementation

--- reduce_implementations/2_interleaved_addressing_with_bank_conflicts.cu
+++ reduce_implementations/2_interleaved_addressing_with_bank_conflicts.cu
@@ -11,9 +11,9 @@ __global__ void reduce_using_2_interleaved_addressing_with_bank_conflicts(int *i
 
     // Do reduction in shared memory.
     for(unsigned int amountOfElementsReduced = 1; amountOfElementsReduced < blockDim.x; amountOfElementsReduced *= 2) {
-        int index = 2 * amountOfElementsReduced * threadIndex;
+        int index = 2 * amountOfElementsReduced * threadIndex;  // This indexing may produce memory bank conflicts.
         if (index < blockDim.x) {
-            sharedData[index] += sharedData[index + amountOfElementsReduced];  // This indexing is producing memory bank conflicts.
+            sharedData[index] += sharedData[index + amountOfElementsReduced];
         }
         __syncthreads();
     }
@@ -24,6 +24,16 @@ __global__ void reduce_using_2_interleaved_addressing_with_bank_conflicts(int *i
 
 /*
 
-Bank conflicts explanation here.
+Memory banks are divisions of the shared memory in CUDA.
+Memory bank conflicts happen when two threads in the same warp try to access shared memory addresses mapping to the same memory bank.
+
+When memory bank conflicts occur, the memory accesses are serialized.
+This, of course, slows performance.
+
+The memory bank conflicts are produced in the line when indexing with (int index = 2 * amountOfElementsReduced * threadIndex).
+Accessing shared memory in indexes (2 * amountOfElementsReduced * threadIndex) and that plus (amountOfElementsReduced) may lead to conflicts.
+This is because amountOfElementsReduced is growing each iteration, which can cause a thread to access another thread's memory bank (even in the same warp).
+The solution would be to make the addressing sequential instead of interleaved, reducing the distance between both memory accesses in each iteration.
+This helps reduce the likelyhood of accessing a memory bank belonging to another thread in the same warp.
 
 */
