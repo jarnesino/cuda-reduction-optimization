8694eceab75b9b7e68ca91f19721b5a3a2fa5f17
Julian Arnesino <jarnesino@eryx.co>
2024-07-22 21:11:30 +0000

    Add explanation for the sixth implementation

--- reduce_implementations/6_complete_loop_unrolling_with_one_reduction.cu
+++ reduce_implementations/6_complete_loop_unrolling_with_one_reduction.cu
@@ -44,3 +44,11 @@ __device__ void warpReduce(volatile int* sharedData, int threadBlockIndex) {
     if (blockSize >= 4) sharedData[threadBlockIndex] += sharedData[threadBlockIndex + 2];
     if (blockSize >= 2) sharedData[threadBlockIndex] += sharedData[threadBlockIndex + 1];
 }
+
+/*
+
+Many kernels can be launched, but a GPU has a limited amount of blocks in a grid.
+When that amount (e.g. 16) is reached, the rest of the kernel executions are serialized.
+Knowing this, we can use it to our advantage and reduce the data to a grid-sized array before executing the grid-wide reduction.
+
+*/
