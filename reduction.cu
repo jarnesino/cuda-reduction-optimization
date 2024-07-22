8cce267bece704d7756344c0ae066b1d37bfe4d6
Julian Arnesino <jarnesino@eryx.co>
2024-07-22 21:20:17 +0000

    WIP - Fix seventh implementation

--- reduction.cu
+++ reduction.cu
@@ -27,7 +27,7 @@ int main() {
     reduce(4, reduce_using_4_first_add_during_load_with_loop_overhead, 2, testingData, dataSize, startEvent, stopEvent);
     reduce(5, reduce_using_5_loop_unrolling_only_at_warp_level_iterations, 2, testingData, dataSize, startEvent, stopEvent);
     reduce(6, reduce_using_6_complete_loop_unrolling_with_one_reduction, 2, testingData, dataSize, startEvent, stopEvent);
-    reduce(7, reduce_using_7_multiple_reduce_operations_per_thread_iteration, 2, testingData, dataSize, startEvent, stopEvent);
+    reduce7(7, reduce_using_7_multiple_reduce_operations_per_thread_iteration, 2, testingData, dataSize, startEvent, stopEvent);
 
     cudaEventDestroy(startEvent);
     cudaEventDestroy(stopEvent);
@@ -90,13 +90,67 @@ void reduce(
     cudaFree(deviceOutputData);
 }
 
+void reduce7(
+    const int implementationNumber,
+    reduceImplementationFunction implementation,
+    const int blockSizedChunksReducedPerBlock,
+    int* inputData,
+    const int dataSize,
+    cudaEvent_t startEvent,
+    cudaEvent_t stopEvent
+) {
+    const int threadsPerBlock = BLOCK_SIZE;
+    const size_t dataSizeInBytes = dataSize * sizeof(int);
+    int amountOfBlocks = 16;
+
+    int *deviceInputData, *deviceOutputData;
+    cudaMalloc((void **)&deviceInputData, dataSizeInBytes);
+    cudaMalloc((void **)&deviceOutputData, amountOfBlocks * sizeof(int) * 2);  // Allocate double the memory for use in subsequent layers.
+    cudaMemcpy(deviceInputData, inputData, dataSizeInBytes, cudaMemcpyHostToDevice);
+
+    int remainingElements = dataSize;
+    int *inputPointer = deviceInputData;
+    int *outputPointer = deviceOutputData;
+    const size_t sharedMemSize = threadsPerBlock * sizeof(int);
+
+    // Record the start event.
+    cudaEventRecord(startEvent, 0);
+
+    // Launch kernel for each block.
+    while (remainingElements > 1) {
+        amountOfBlocks = min(amountOfBlocksForReduction(remainingElements, threadsPerBlock, blockSizedChunksReducedPerBlock), 16);
+        implementation<<<amountOfBlocks, threadsPerBlock, sharedMemSize>>>(inputPointer, outputPointer, remainingElements);
+        cudaDeviceSynchronize();
+
+        remainingElements = amountOfBlocks;
+        inputPointer = outputPointer;
+        outputPointer += remainingElements;
+    }
+
+    // Record the stop event and wait for it to complete.
+    cudaEventRecord(stopEvent, 0);
+    cudaEventSynchronize(stopEvent);
+
+    int finalResult;
+    cudaMemcpy(&finalResult, inputPointer, sizeof(int), cudaMemcpyDeviceToHost);
+
+    float elapsedTimeInMilliseconds;
+    cudaEventElapsedTime(&elapsedTimeInMilliseconds, startEvent, stopEvent);
+
+    printImplementationData(implementationNumber, elapsedTimeInMilliseconds, finalResult);
+
+    cudaFree(deviceInputData);
+    cudaFree(deviceOutputData);
+}
+
+
 int amountOfBlocksForReduction(const int dataSize, const int threadsPerBlock, const int blockSizedChunksReducedPerBlock) {
     return (dataSize + threadsPerBlock * blockSizedChunksReducedPerBlock - 1) / (threadsPerBlock * blockSizedChunksReducedPerBlock);
 }
 
 void printImplementationData(const int implementationNumber, float elapsedTimeInMilliseconds, int result) {
     std::cout << "*** Implementation number: " << implementationNumber;
-    std::cout << "\t Elapsed time: " << elapsedTimeInMilliseconds;
+    std::cout << "\t Elapsed time: " << elapsedTimeInMilliseconds << " ms";
     std::cout << "\t Reduction result: " << result << std::endl;
 }
 
