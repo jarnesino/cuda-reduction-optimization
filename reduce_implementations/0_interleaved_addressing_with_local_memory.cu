02f23a0abb70f112bae7e103bee1c5857760e9e8
Julian Arnesino <jarnesino@eryx.co>
2024-07-22 19:48:26 +0000

    Add explanation for reduce implementation using local memory

--- reduce_implementations/0_interleaved_addressing_with_local_memory.cu
+++ reduce_implementations/0_interleaved_addressing_with_local_memory.cu
@@ -8,7 +8,7 @@ __global__ void reduce_using_0_interleaved_addressing_with_local_memory(int *inp
     unsigned int threadIndex = blockIndex * blockSize + threadBlockIndex;
     __syncthreads();
 
-    // Do reduction in shared memory.
+    // Do reduction in global memory. Causes slow access speeds.
     for(unsigned int amountOfElementsReduced = 1; amountOfElementsReduced < blockSize; amountOfElementsReduced *= 2) {
         if (threadBlockIndex % (2 * amountOfElementsReduced) == 0) {
             inputData[threadIndex] += inputData[threadIndex + amountOfElementsReduced];
@@ -22,6 +22,13 @@ __global__ void reduce_using_0_interleaved_addressing_with_local_memory(int *inp
 
 /*
 
+There's three (relevant) types of memory in CUDA.
+- Global memory : accessible by all threads in all blocks.
+- Shared memory: accessible by all threads in a single block.
+- Local memory: accessible by a single thread.
+The access speeds in global memory are of slower bandwidth and higher latency compared to the ones in shared memory.
 
+The pointer (inputData) leads to an array in global memory.
+Since we access and modify the array in global memory, the access speed is unnecessarily limited, making the kernel execute slower than necessary.
 
 */
