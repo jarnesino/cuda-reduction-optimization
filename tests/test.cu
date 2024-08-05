09634ef19ef4d2155c7c0d5640d5a6d25280404b
Julian Arnesino <julianezequielarnesino@gmail.com>
2024-08-05 17:16:51 +0000

    Add test with random positive and negative numbers

--- tests/test.cu
+++ tests/test.cu
@@ -2,11 +2,12 @@
 
 #include "doctest.h"
 #include "../reduction.cuh"
+#include <random>
 
 TEST_SUITE("reduction of arrays with different sizes") {
-    unsigned int initializeTestingDataAndGetSum(int *data, unsigned int size) {
+    int initializeTestingDataAndGetSum(int *data, unsigned int size) {
         fillDataWith1s(data, size);
-        unsigned int sum = size;
+        int sum = (int) size;
         return sum;
     }
 
@@ -37,6 +38,37 @@ TEST_SUITE("reduction of arrays with different sizes") {
     TEST_CASE("reduce big arrays") {
         const unsigned int logDataSize = 30;
         testReductionsWithArrayOfLogSize(logDataSize);
+    }
+}
+
+TEST_SUITE("reduction of arrays with random data") {
+    int initializeRandomTestingDataAndGetSum(int *data, unsigned int size) {
+        std::random_device randomSeed;
+        std::mt19937 gen(randomSeed());
+        std::uniform_int_distribution<> uniformDistribution(-2000, 2000);
+
+        int sum = 0;
+        for (unsigned int index = 0; index < size; ++index) {
+            int randomNumber = uniformDistribution(gen);
+            data[index] = randomNumber;
+            sum += randomNumber;
+        }
+        return sum;
+    }
+
+    TEST_CASE("reduce arrays with random positive and negative integers") {
+        const unsigned int logDataSize = 11;
+        const unsigned int dataSize = 1 << logDataSize;
+        int *testingData = new int[dataSize];
+        int expectedSum = initializeRandomTestingDataAndGetSum(testingData, dataSize);
+
+        for (const auto &reduceImplementation: reduceImplementations) {
+            ReductionResult reductionResult = reduceAndMeasureTime(
+                    reduceImplementation, testingData, dataSize
+            );
+
+            CHECK_EQ(reductionResult.value, expectedSum);
+        }
     }
 }
 
