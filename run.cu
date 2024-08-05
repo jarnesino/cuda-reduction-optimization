e3e819adf90fe4950963eb7d383c0e960f1f443d
Julian Arnesino <julianezequielarnesino@gmail.com>
2024-08-05 19:17:03 +0000

    Rewrite main reduction files to run benchmarks

--- run.cu
+++ run.cu
@@ -7,7 +7,7 @@ https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
 
 */
 
-void printImplementationData(unsigned int implementationNumber, float elapsedTimeInMilliseconds, int result);
+void printImplementationData(unsigned int implementationNumber, float elapsedTimeInMilliseconds);
 
 int main() {
     const unsigned int logDataSize = 30;
@@ -15,18 +15,27 @@ int main() {
     int *testingData = new int[dataSize];
     initializeTestingDataIn(testingData, dataSize);
 
+    float elapsedTimeInReduction[AMOUNT_OF_IMPLEMENTATIONS];
+
     for (unsigned int implementationIndex = 0; implementationIndex < AMOUNT_OF_IMPLEMENTATIONS; implementationIndex++) {
-        ReductionResult reductionResult = reduceAndMeasureTime(
-                reduceImplementations[implementationIndex], testingData, dataSize
-        );
-        printImplementationData(implementationIndex, reductionResult.elapsedTimeInMilliseconds, reductionResult.value);
+        const unsigned int SAMPLE_SIZE = 20;
+        float sumOfElapsedTimesInMilliseconds = 0;
+
+        for (unsigned int sampleIndex = 0; sampleIndex < SAMPLE_SIZE; sampleIndex++) {
+            ReductionResult reductionResultForImplementation = reduceAndMeasureTime(
+                    reduceImplementations[implementationIndex], testingData, dataSize
+            );
+            sumOfElapsedTimesInMilliseconds += reductionResultForImplementation.elapsedTimeInMilliseconds;
+        }
+
+        elapsedTimeInReduction[implementationIndex] = sumOfElapsedTimesInMilliseconds / SAMPLE_SIZE;
+        printImplementationData(implementationIndex, elapsedTimeInReduction[implementationIndex]);
     }
 
     return EXIT_SUCCESS;
 }
 
-void printImplementationData(const unsigned int implementationNumber, float elapsedTimeInMilliseconds, int result) {
+void printImplementationData(const unsigned int implementationNumber, float elapsedTimeInMilliseconds) {
     printf("*** Implementation number: %d", implementationNumber);
-    printf("\t Elapsed time: %f ms", elapsedTimeInMilliseconds);
-    printf("\t Reduction result: %d\n", result);
+    printf("\t Elapsed time: %f ms\n", elapsedTimeInMilliseconds);
 }
