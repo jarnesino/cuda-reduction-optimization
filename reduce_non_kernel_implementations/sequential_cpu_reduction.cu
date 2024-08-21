c67eb4750e893d30604932faf91ed6740b471522
Julian Arnesino <julianezequielarnesino@gmail.com>
2024-08-21 19:22:00 +0000

    Reformat

--- reduce_non_kernel_implementations/sequential_cpu_reduction.cu
+++ reduce_non_kernel_implementations/sequential_cpu_reduction.cu
@@ -1,4 +1,6 @@
-int reduceWithCPU(int *inputData, unsigned int size) {int sum = 0;
+int reduceWithCPU(int *inputData, unsigned int size) {
+    int sum = 0;
+
     for (unsigned int index = 0; index < size; index++) {
         sum += inputData[index];
     }
--- reduce_non_kernel_implementations/thrust_reduction.cu
+++ reduce_non_kernel_implementations/thrust_reduction.cu
@@ -2,7 +2,6 @@
 #include <thrust/reduce.h>
 
 int reduceWithThrust(int *inputData, unsigned int size) {
-
     thrust::device_vector<int> deviceInputData(inputData, inputData + size);
 
     return thrust::reduce(deviceInputData.begin(), deviceInputData.end(), 0, thrust::plus<int>());
